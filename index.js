f7bebb68b0fc07c7fd4e2da82fc8fb37a6f79f53
Ashish Garg <hawkeye@Ashishs-Mac-mini.local>
2023-03-15 19:36:50 +0530

    cors issue fix

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ app.use(express.json())
 // const swaggerUi = require('swagger-ui-express');
 // swaggerDocument = require('./swagger.json');
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 const appVersion = '/api/v1';
 
 app.use(`${appVersion}/user`, userRoutes)
