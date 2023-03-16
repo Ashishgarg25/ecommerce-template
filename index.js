c4a54b8a9e1a1f1438429320097fb8ef29006635
Ashish Garg <hawkeye@Ashishs-Mac-mini.local>
2023-03-17 02:11:20 +0530

    minor fix

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require("express");
-const http = require('http');
+const http = require("http");
+const mongoose = require("mongoose");
+
 // ENV Config
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-// Mongo DB Connection
-const { ConnectToMongoDB } = require('./config/database');
-ConnectToMongoDB();
+// // Mongo DB Connection
+// const { ConnectToMongoDB } = require('./config/database');
+// ConnectToMongoDB();
+
+dotenv.config();
 
 // Import Routes
 // const userRouter = require('./routes/user');
-const userRoutes = require('./routes/Users');
-const shopRoutes = require('./routes/Shop');
-const productRoutes = require('./routes/Products');
-const orderRoutes = require('./routes/Order');
-const blogRoutes = require('./routes/Blog');
+const userRoutes = require("./routes/Users");
+const shopRoutes = require("./routes/Shop");
+const productRoutes = require("./routes/Products");
+const orderRoutes = require("./routes/Order");
+const blogRoutes = require("./routes/Blog");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+const MongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 8000;
+
+mongoose
+  .connect(MongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(port, () => console.log(`Server started on PORT ${port}`));
+    console.log("MongoDB Connection Successfully!");
+  })
+  .catch((err) => console.log(`Error ${err}`));
 
 // const swaggerUi = require('swagger-ui-express');
 // swaggerDocument = require('./swagger.json');
@@ -35,27 +53,25 @@ app.use((req, res, next) => {
   next();
 });
 
-const appVersion = '/api/v1';
+const appVersion = "/api/v1";
 
-app.use(`${appVersion}/user`, userRoutes)
-app.use(`${appVersion}/product`, productRoutes)
-app.use(`${appVersion}/order`, orderRoutes)
-app.use(`${appVersion}/shop`, shopRoutes)
-app.use(`${appVersion}/post`, blogRoutes)
+app.use(`${appVersion}/user`, userRoutes);
+app.use(`${appVersion}/product`, productRoutes);
+app.use(`${appVersion}/order`, orderRoutes);
+app.use(`${appVersion}/shop`, shopRoutes);
+app.use(`${appVersion}/post`, blogRoutes);
 
-app.get('/', function(req, res){
-  res.send(`<body style='background-color: #1E293B; padding: 16px'><h4 style='font-weight: 500; color: #FFFFFF'>Application in Development. Ecommerce Template </h4></body>`);
+app.get("/", function (req, res) {
+  res.send(
+    `<body style='background-color: #1E293B; padding: 16px'><h4 style='font-weight: 500; color: #FFFFFF'>Application in Development. Ecommerce Template </h4></body>`
+  );
 });
 
 // app.use(
 //     '/api-docs',
-//     swaggerUi.serve, 
+//     swaggerUi.serve,
 //     swaggerUi.setup(swaggerDocument)
 // );
-
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log(`Server started on PORT ${port}`));
-
 
 // FOR PRODUCTION
 // http.createServer(app).listen(port, (req, res) => {
